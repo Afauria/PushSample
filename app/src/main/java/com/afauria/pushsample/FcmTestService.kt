219520c252d1a13093a672f759baa4f9fd161a1a
zhangweiyue <zhangweiyue@cvte.com>
2021-02-04 10:15:43 +0800

    [feature] add notification toast
    [what] 收到消息之后弹出toast
    [why] TV上没有通知栏，需要自定义弹窗
    [how] 弹出自定义Toast。后台弹窗问题小结
    [check] none

--- app/src/main/java/com/afauria/pushsample/FcmTestService.kt
+++ app/src/main/java/com/afauria/pushsample/FcmTestService.kt
@@ -1,6 +1,11 @@
 package com.afauria.pushsample
 
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -28,8 +33,16 @@ class FcmTestService : FirebaseMessagingService() {
         }
 
         // Check if message contains a notification payload.
-        if (remoteMessage.notification != null) {
+        remoteMessage.notification?.let {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body)
+            Handler(Looper.getMainLooper()).post {
+                Toast(this).apply {
+                    view = LayoutInflater.from(this@FcmTestService)
+                        .inflate(R.layout.layout_notification, null, false)
+                    setGravity(Gravity.END or Gravity.BOTTOM, 30, 30)
+                    duration = Toast.LENGTH_LONG
+                }.show()
+            }
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
